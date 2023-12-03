c0da466bde04ae2abb2db93126463828bc4d41ce
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-12-04 00:00:17 +0200

    fix user file

---
+++ projectPL2/User.java
@@ -0,0 +1,69 @@
+
+
+/**
+ *
+ * @author user
+ */
+public class User {
+  private int id;
+  private String name;
+  private String email;
+  private String password;
+  private String role; // employee or customer
+
+  // constructor
+  User(int id, String name, String email, String password, String role) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+  }
+
+   // getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    // setters with validation
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        // Basic email validation (you can enhance this based on your requirements)
+        if (email != null && email.contains("@")) {
+            this.email = email;
+        } else {
+            throw new IllegalArgumentException("Invalid email format");
+        }
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
